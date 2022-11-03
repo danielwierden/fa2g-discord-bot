dd0d04f69e946d7d35d3acb100461c2391565103
Daniel Dabrowski <danielwierden@outlook.com>
2022-11-04 00:30:06 +0100

    added new blackie for test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on("voiceStateUpdate", async (oldState, newState) => {
         if (oldState.channelID !== newState.channelID) {
             // Daniel
             if (newState.member.id === '215177142613442570') {
-                await play(newState.member.voice.channel, 'daniel2.mp3', .4)
+                await play(newState.member.voice.channel, 'kerstBlackie.mp3', .4)
             }
 
             // Sam
